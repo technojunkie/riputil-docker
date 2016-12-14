d5c6ee5f98e6ed069deebe9b856430f7727dd707
technojunkie <zjunkie@gmail.com>
2016-12-13 19:01:46 -0700

    added back license step

--- start.sh
+++ start.sh
@@ -6,5 +6,11 @@ if [ "$PASSWORD" != "" ]; then
   echo "$USER:$PASSWORD" | chpasswd
 fi
 
+#Adding License Key
+if [! -e /root/.makemkv ]; then
+  mkdir /root/.makemkv
+fi
+echo "app_Key =\"$LICENSE\"" > /root/.makemkv/settings.conf
+
 # Starting ssh daemon
 /usr/sbin/sshd -D
